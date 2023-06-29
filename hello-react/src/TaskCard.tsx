e72582aed5da7222a7f2fb59d97a643e9a1c9f98
Shailesh Aanand <anaandshailu@gmail.com>
2023-06-29 11:51:47 +0530

    migrate to typescript

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -1,4 +1,10 @@
-const TaskCard = ({ title, dueDate, assigneeName, completedAtDate }) => {
+import React from "react";
+const TaskCard = ({ title, dueDate, assigneeName, completedAtDate }: {
+  title: string,
+  dueDate?: string,
+  assigneeName: string,
+  completedAtDate?: string
+}) => {
   return (
     <div className="border-2 border-black rounded p-3">
       <h2 className="text-xl font-bold mb-1">{title}</h2>
