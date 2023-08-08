757b992a8ff6c7fc6b744364ba084cfb4ffc5386
Shailesh Aanand <anaandshailu@gmail.com>
2023-08-08 18:25:36 +0530

    Programmatic Navigation

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,15 +1,33 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import TaskListPage from "./pages/TaskListPage";
 import TaskDetailsPage from "./pages/TaskDetailsPage";
+import Signin from "./pages/Signin";
+import ProtectedRoute from "./ProtectedRoute";
 import Layout from "./Layout";
 
 const router = createBrowserRouter([
   {
-    element: <Layout />,
+    path: "/",
+    element: <Navigate to="/signin" replace />,
+  },
+  {
+    path: "/signin",
+    element: <Signin />,
+  },
+  {
+    element: (
+      <ProtectedRoute>
+        <Layout />
+      </ProtectedRoute>
+    ),
     children: [
       {
-        path: "/",
+        path: "home",
         element: <HomePage />,
       },
       {
