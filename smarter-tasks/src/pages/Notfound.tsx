791a059f5ab783d15d8e4b664c06e26e7f28450a
Shailesh Aanand <anaandshailu@gmail.com>
2023-08-08 20:37:30 +0530

    add #backToHomeButton id to back to home button

--- smarter-tasks/src/pages/Notfound.tsx
+++ smarter-tasks/src/pages/Notfound.tsx
@@ -7,6 +7,7 @@ const NotFound: React.FC = () => {
       <p>404 Not Found</p>
       <Link
         to="/home"
+        id="backToHomeButton"
         className="inline-block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         Go Home
