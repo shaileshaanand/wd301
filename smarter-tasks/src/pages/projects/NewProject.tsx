afabc0ac14616313aa42646097b83dbf83cce142
Shailesh Aanand <anaandshailu@gmail.com>
2023-09-26 20:25:53 +0530

    19664

--- smarter-tasks/src/pages/projects/NewProject.tsx
+++ smarter-tasks/src/pages/projects/NewProject.tsx
@@ -1,5 +1,10 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { API_ENDPOINT } from "../../config/constants";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+};
 
 // Next we will import Fragment and useState component from 'react'
 
@@ -9,6 +14,11 @@ const NewProject = () => {
 
   let [isOpen, setIsOpen] = useState(false);
   const [name, setName] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
 
   // Then we add the openModal function.
   // If you don't know, Modal and Dialog are almost same thing.
@@ -21,8 +31,11 @@ const NewProject = () => {
   const closeModal = () => {
     setIsOpen(false);
   };
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    // Let's remove event.PreventDefault()
+    // Next, we will destructure the data object to access name field value
+    const { name } = data;
+
     const token = localStorage.getItem("authToken") ?? "";
     try {
       setIsOpen(false);
@@ -97,18 +110,17 @@ const NewProject = () => {
                     Create new project
                   </Dialog.Title>
                   <div className="mt-2">
-                    <form onSubmit={handleSubmit}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                       <input
                         type="text"
-                        required
                         placeholder="Enter project name..."
                         autoFocus
-                        name="name"
-                        id="name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        className="w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue"
+                        {...register("name", { required: true })}
+                        className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
+                          errors.name ? "border-red-500" : ""
+                        }`}
                       />
+                      {errors.name && <span>This field is required</span>}
                       <button
                         type="submit"
                         className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 mr-2 text-sm font-medium text-white hover:bg-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
